c32a54fa9dd073f40501640e3ba4087f4e957b25
Huijie Wei <huijiewei@outlook.com>
2023-03-25 12:10:57 +0800

    chore: add typescript paths improve DX

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,31 +1,44 @@
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+import { defineConfig, type UserConfig } from 'vite';
 import resolid from '@resolid/run/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    resolid({
-      manualChunks(id) {
-        if (id.includes('/node_modules/solid-js/') || id.includes('/node_modules/@solidjs/')) {
-          return 'solid';
-        }
+export default defineConfig(({ command }) => {
+  const isBuild = command == 'build';
 
-        if (id.includes('/node_modules/@resolid/') && !id.includes('/node_modules/@resolid/run/')) {
-          return 'resolid';
-        }
+  const config: UserConfig = {
+    plugins: [
+      !isBuild && tsconfigPaths(),
+      resolid({
+        manualChunks(id) {
+          if (id.includes('/node_modules/solid-js/') || id.includes('/node_modules/@solidjs/')) {
+            return 'solid';
+          }
 
-        if (id.includes('/node_modules/')) {
-          return 'vendor';
-        }
+          if (id.includes('/node_modules/@resolid/') && !id.includes('/node_modules/@resolid/run/')) {
+            return 'resolid';
+          }
 
-        if (id.includes('/packages/') && !id.includes('/packages/run/')) {
-          return 'resolid';
-        }
-      },
-    }),
-  ],
-  build: {
-    minify: true,
-  },
+          if (id.includes('/node_modules/')) {
+            return 'vendor';
+          }
+
+          if (id.includes('/packages/') && !id.includes('/packages/run/')) {
+            return 'resolid';
+          }
+        },
+      }),
+    ],
+    build: {
+      minify: true,
+    },
+  };
+
+  if (isBuild) {
+    config.resolve = {
+      alias: [{ find: '~', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+    };
+  }
+
+  return config;
 });
