dab719857033cb2ffb6c0969971c2ccc18c3ec37
Huijie Wei <huijiewei@outlook.com>
2023-03-25 11:30:29 +0800

    feat(utils): add object and string util

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,3 +2,5 @@ export * from './number';
 export * from './function';
 export * from './types';
 export * from './array';
+export * from './string';
+export * from './object';
---
+++ packages/utils/src/string/index.ts
@@ -0,0 +1,3 @@
+export const isString = (value: unknown): value is string => {
+  return Object.prototype.toString.call(value) === '[object String]';
+};
